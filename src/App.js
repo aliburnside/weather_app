6f2f2d288dabee39696ff8a210c32e139ccca2df
Alexandra Burnside <burnsideally@gmail.com>
2024-03-17 15:13:40 -0400

    react app init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import WeatherApp from "./Componants/WeatherApp/WeatherApp";
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp></WeatherApp>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componants/WeatherApp/WeatherApp.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./WeatherApp.css";
+
+import search_icon from "../Asset";
+
+const WeatherApp = () => {
+  return <div></div>;
+};
+export default WeatherApp;
