c1826c8c17fb2e608f81e5d0bbd88e24b5bb985f
Alexandra Burnside <burnsideally@gmail.com>
2024-03-17 15:43:57 -0400

    assets and search bar add

--- src/Componants/WeatherApp/WeatherApp.jsx
+++ src/Componants/WeatherApp/WeatherApp.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 import "./WeatherApp.css";
 
-import search_icon from "../Asset";
+import search from "../Asset/search.png";
+import clear from "../Asset/clear.png";
+import cloud from "../Asset/cloud.png";
+import drizzle from "../Asset/drizzle.png";
+import humidity from "../Asset/humidity.png";
+import rain from "../Asset/rain.png";
+import snow from "../Asset/snow.png";
+import wind from "../Asset/wind.png";
 
 const WeatherApp = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="container">
+        <div className="top-bar">
+          <input type="text" className="cityInput" placeholder="Search" />
+          <div className="search-icon">
+            <img src={search} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default WeatherApp;
